ecb3a3224936d2b2456cb32e82f321e5b37c3f60
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2020-10-13 12:36:11 -0500

    Add files via upload

--- dog.js
+++ dog.js
@@ -1,40 +1,77 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage(breed) {
+  fetch('https://dog.ceo/api/breed/' + breed +'/images/random') /*  https://dog.ceo/api/breed/hound/images/random    */
+    
+
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .catch(error => alert("Something went wrong!"));
+
+   
+   console.log(breed);
+
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
+
+  if (responseJson.status == "success") {
+  $('.js-add-img').append(
+    `<div class="js-box">
+    	<img src="${responseJson.message}" class="results-img">
+    </div>`
   )
+}
+  else if (responseJson.status == "error") {
+
+  console.log("If else error worked")
+}
+  console.log(responseJson.message);
+  console.log(responseJson.code);
+  console.log(responseJson.status);
   //display the results section
-  $('.results').removeClass('hidden');
+  //$('.results').removeClass('hidden');
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
 
-    let totalDogPics = document.getElementById('numdogs').val(); // Problem on this line
-    console.log(totalDogPics);
+    
+    let totalDogPics = $("select#numdogs").val();
 
-    getDogImage();
+    let convertTotalDogPics = parseInt(totalDogPics, 10);
+ 
+
+    let dogBreed = $("input#js-breed:text").val();
+    //console.log(dogBreed);
+    
+    for (let i = 1; i <= convertTotalDogPics; i++) {
+
+
+			getDogImage(dogBreed);
+			
+    
+    }
+
+
   });
 }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
+  //notFound();
+
 });
 
+function notFound() {
 
+  console.log("Image not found");
+}
 
 
 let htmlD = '';	
